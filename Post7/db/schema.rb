2b5171218cd752402d9af124e34e3fec59955a66
Bablu <bk147411@gmail.com>
2023-01-04 19:50:01 +0000

    abcd

--- Post7/db/schema.rb
+++ Post7/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_08_070234) do
+ActiveRecord::Schema.define(version: 2022_12_31_184037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,10 @@ ActiveRecord::Schema.define(version: 2022_12_08_070234) do
     t.string "ten_day_price"
     t.string "twenty_day_price"
     t.string "thirty_day_price"
+    t.string "email_inq"
+    t.string "phone_inq"
+    t.string "listing_save"
+    t.string "listing_view"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
